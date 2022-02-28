abe664a9c8967bd57196653f3e65cc879a8068a3
Zech Zimmerman <hi@zech.codes>
2022-02-27 20:15:53 -0500

    Handle for hook inheritance

--- bevy/app/agents/hooks.py
+++ bevy/app/agents/hooks.py
@@ -1,14 +1,34 @@
 from __future__ import annotations
 from collections import defaultdict
 from typing import Callable, cast, ParamSpec, TypeVar
+from copy import deepcopy
+from inspect import getmro
 
 
 P = ParamSpec("P")
 R = TypeVar("R")
 
 
+class HookClassRepository:
+    def __init__(self):
+        self.repo = {}
+
+    def __get__(self, _, owner: Hookable) -> dict[str, set[str]]:
+        if id(owner) not in self.repo:
+            self.repo[id(owner)] = self.build_hook_dict(owner)
+
+        return self.repo[id(owner)]
+
+    def build_hook_dict(self, owner):
+        for super_ in getmro(owner):
+            if super_ is not owner and hasattr(super_, "__bevy_hooks__"):
+                return deepcopy(super_.__bevy_hooks__)
+
+        return defaultdict(set)
+
+
 class Hookable:
-    __bevy_hooks__ = defaultdict(set)
+    __bevy_hooks__: dict[str, set[str]] = HookClassRepository()
 
 
 class Hook:
