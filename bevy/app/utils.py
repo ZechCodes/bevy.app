353c3ea8fbeac0516b2fc9c2c6dfaee1ce5bf68b
Zech Zimmerman <hi@zech.codes>
2022-03-06 17:53:45 -0500

    Add apply function

--- bevy/app/utils.py
+++ bevy/app/utils.py
@@ -5,6 +5,20 @@ from inspect import isawaitable
 from typing import Awaitable, Callable, Iterable, TypeVar
 
 
+T = TypeVar("T")
+def apply(
+    func: Callable[[T], Awaitable[None, None, None] | None], iterable: Iterable[T]
+) -> asyncio.Future:
+    """Simple function for applying a callable to each item in an iterable."""
+    tasks = []
+    for item in iterable:
+        ret = func(item)
+        if isawaitable(ret):
+            tasks.append(ret)
+
+    return asyncio.gather(*tasks)
+
+
 class AwaitAllNewTasks:
     def __init__(self, loop: asyncio.BaseEventLoop | None = None):
         self.loop = loop
